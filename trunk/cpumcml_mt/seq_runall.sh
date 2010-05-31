044151d92ae975d601093f23b79b03dcc6d49835
gpumcml <gpumcml@66a1fcd9-0dc7-7a4d-013d-e7169809e925>
2010-05-31 20:51:11 +0000

    changed order slightly

--- trunk/cpumcml_mt/seq_runall.sh
+++ trunk/cpumcml_mt/seq_runall.sh
@@ -1,12 +1,12 @@
+./mcml input/7layerskin_600nm_100M_run1.mci  
 ./mcml gridres/run1/1.mci  
 ./mcml gridres/run1/10.mci 
 ./mcml gridres/run1/100.mci 
 ./mcml gridres/run1/1000.mci 
-./mcml input/7layerskin_600nm_100M_run1.mci  
+./mcml input/7layerskin_600nm_100M_run2.mci  
 ./mcml gridres/run2/1.mci  
 ./mcml gridres/run2/10.mci   
 ./mcml gridres/run2/100.mci  
 ./mcml gridres/run2/1000.mci  
-./mcml input/7layerskin_600nm_100M_run2.mci  
 ./mcml input/7layerskin_600nm_100M_run3.mci 
 ./mcml input/7layerskin_600nm_100M_run4.mci 
